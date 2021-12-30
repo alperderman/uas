be3f185f083ede6799efd94f118a2a98bd2ef217
alperderman <73118676+alperderman@users.noreply.github.com>
2021-12-30 20:22:12 +0300

    Update config.php

--- models/config.php
+++ models/config.php
@@ -3,8 +3,6 @@
 class config {
     public $sessionLimits = ["min"=>24, "max"=>32];
     public $chainLimits = ["min"=>32, "max"=>48];
-    public $usernameLimits = [4, 16];
-    public $passwordLimits = [4, 24];
     public $usernameRegex = '/^[a-zA-Z0-9]{4,16}$/';
     public $passwordRegex = '/^[a-zA-Z0-9]{4,24}$/';
     public $defaultSession = 'POST.s';
