f0f377fddee8f90114c5a7d51c08c2ef2fa9ace3
alperderman <73118676+alperderman@users.noreply.github.com>
2022-06-20 10:10:04 +0300

    removed the remnant functions

--- util.php
+++ util.php
@@ -1,29 +1,6 @@
 <?php
 
 class util {
-
-    function limitData($data, $limit) {
-        $countData = count($data);
-        $countLimit = count($limit);
-        $result = [];
-        if($countData == $countLimit){
-            for($i=0;$i < $countData;$i++){
-            if($data[$i] == null || $data[$i] == ""){
-                $result = false;
-                break;
-            } else {
-                    if($limit[$i] != false && strlen($data[$i]) > $limit[$i]){
-                        array_push($result, substr($data[$i], 0, $limit[$i]));
-                    } else {
-                        array_push($result, $data[$i]);
-                    }
-                }
-            }
-        } else {
-            $result = false;
-        }
-        return $result;
-    }
     
     function multiPregMatch($regs, $args) {
         $result = true;
@@ -53,32 +30,6 @@ class util {
             $result = false;
         }
         return $result;
-    }
-
-    function crypto_rand_secure($min, $max) {
-        $range = $max - $min;
-        if ($range < 1) return $min;
-        $log = ceil(log($range, 2));
-        $bytes = (int) ($log / 8) + 1;
-        $bits = (int) $log + 1;
-        $filter = (int) (1 << $bits) - 1;
-        do {
-            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
-            $rnd = $rnd & $filter;
-        } while ($rnd > $range);
-        return $min + $rnd;
-    }
-
-    function newToken($length){
-        $token = "";
-        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
-        $codeAlphabet.= "0123456789";
-        $max = strlen($codeAlphabet);
-        for ($i=0; $i < $length; $i++) {
-            $token .= $codeAlphabet[$this->crypto_rand_secure(0, $max-1)];
-        }
-        return $token;
     }
     
 }
